a7877628ce274163f23fb75cf061cacbf0c4d067
user <user@sd213065.ming.ab.nthu.edu.tw>
2023-01-12 01:25:56 +0800

    map_component done

--- react_adb_frontend/src/pages/Map.js
+++ react_adb_frontend/src/pages/Map.js
@@ -1,45 +1,57 @@
 import React from "react";
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import Qform from "../components/Qform";
+import Queryform from "../components/Queryform";
 import  { useEffect, useState } from "react";
 import Mapitems from "../components/Mapitems";
 
 const Map = () => {
+  const queryparam = {
+    startdate:"2011-12-25",
+    enddate :"2011-12-26",
+    item :"orders",
+    district:"台北市"
+  } 
+
+  const handleSave = (values)=>{
+    console.log(values)
+  }
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyDaw6LVFGCqNKw6gDU0YcfPXXszjn6dHVo",
   });
 
-  if (!isLoaded) return <div> Loading... </div>;
+  if (!isLoaded) return <div> Loading Map </div>;
 
   return (
     <>
       <div className="flexbox form">
-        <Qform />
+        <Queryform className="flexbox map" onSave={handleSave} {...{queryparam}}/> 
       </div>
-      <div className="flexbox map">
+
+      {/* <div className="flexbox map">
         <GMap/>
-      </div>
+      </div> */}
     </>
   );
 };
 
-function GMap() {
+// function GMap() {
 
-  const [latt, setLat] = useState(25.033);
-  const [lngg, setLng]   = useState(121.5654);
+//   const [latt, setLat] = useState(25.033);
+//   const [lngg, setLng]   = useState(121.5654);
  
-  return (
-    <GoogleMap
-      zoom={14}
-      center={{ lat:latt, lng:lngg}}
-      mapContainerClassName="map-container"
-    >
-      <Marker position={{ lat: 25.033, lng: 121.5654 }}></Marker>
-      <Marker position={{ lat: 25.038, lng: 121.5654 }}></Marker>
-      <Marker position={{ lat: 25.02, lng: 121.5654 }}></Marker>
-    </GoogleMap>
-  );
-}
+//   return (
+//     <GoogleMap
+//       zoom={14}
+//       center={{ lat:latt, lng:lngg}}
+//       mapContainerClassName="map-container"
+//     >
+//       <Marker position={{ lat: 25.033, lng: 121.5654 }}></Marker>
+//       <Marker position={{ lat: 25.038, lng: 121.5654 }}></Marker>
+//       <Marker position={{ lat: 25.02, lng: 121.5654 }}></Marker>
+//     </GoogleMap>
+//   );
+// }
 
 export default Map;
